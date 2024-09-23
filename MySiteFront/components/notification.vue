c1c9d908b0d5ed14ccb4157f84a0894250708eb9
DaniilRud <vladimirliba@mail.ru>
2024-09-23 17:34:22 +0300

    Add notification component, remake feedback, fixed bidList component

---
+++ MySiteFront/components/notification.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="notification">
+        <span>{{ spanMessage }}</span>
+    </div>
+</template>
+
+
+<script setup>
+import { defineProps } from 'vue'; 
+ 
+const props = defineProps({
+    spanMessage: {
+        type: String
+    }
+})
+
+</script>
+
+
+<style scoped>
+
+.notification {
+  border: 4px solid green;
+  border-radius: 50px;
+  padding: 20px;
+  background-color: rgb(117, 251, 117);
+  box-shadow: 0px 0px 30px 1px green;
+}
+
+.notification span {
+  color: #fff;
+  font-size: 24px;
+  font-weight: bold;
+}
+
+</style>
--- MySiteFront/pages/feedback/index.vue
+++ MySiteFront/pages/feedback/index.vue
@@ -1,7 +1,28 @@
 <template>
-    <div>
-        <myheader></myheader>   
-        <div class="vac">
+  <div>
+    <myheader></myheader>
+    <div class="vac">
+      <div class="hr"></div>
+      <div class="container">
+        <svg
+          style="color: blue; width: 200px; height: 200px"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 496 512"
+        >
+          <path
+            d="M248 8C111 8 0 119 0 256S111 504 248 504 496 393 496 256 385 8 248 8zM363 176.7c-3.7 39.2-19.9 134.4-28.1 178.3-3.5 18.6-10.3 24.8-16.9 25.4-14.4 1.3-25.3-9.5-39.3-18.7-21.8-14.3-34.2-23.2-55.3-37.2-24.5-16.1-8.6-25 5.3-39.5 3.7-3.8 67.1-61.5 68.3-66.7 .2-.7 .3-3.1-1.2-4.4s-3.6-.8-5.1-.5q-3.3 .7-104.6 69.1-14.8 10.2-26.9 9.9c-8.9-.2-25.9-5-38.6-9.1-15.5-5-27.9-7.7-26.8-16.3q.8-6.7 18.5-13.7 108.4-47.2 144.6-62.3c68.9-28.6 83.2-33.6 92.5-33.8 2.1 0 6.6 .5 9.6 2.9a10.5 10.5 0 0 1 3.5 6.7A43.8 43.8 0 0 1 363 176.7z"
+            fill="blue"
+          ></path>
+        </svg>
+        <a
+          class="tg"
+          target="_blank"
+          href="https://web.telegram.org/k/#@SIMBA1111"
+          >Telegram</a
+        >
+      </div>
+    </div>
+    <!-- <div class="vac">
           <div class="hr"></div>
           <form @submit.prevent class="login-form" action="">
             <email-field v-model="user.email"/>
@@ -11,83 +32,77 @@
             </div>
             </form>
             <div style="color: rgb(0, 255, 119);font-size: 30px;">{{ seccessfully }}</div>
-        </div>
-    </div>
+        </div> -->
+  </div>
 </template>
-  
+
 <script setup>
-import emailField from '../../components/emailField.vue';
-import MessageField from '../../components/messageField.vue';
-import useFormValidation from '~/modules/useFormValidation';
-import useSubmitButtonState from '~/modules/useSubmitButtonState'
+import emailField from "../../components/emailField.vue";
+import MessageField from "../../components/messageField.vue";
+import useFormValidation from "~/modules/useFormValidation";
+import useSubmitButtonState from "~/modules/useSubmitButtonState";
 import { url } from "../../MyConstants.vue";
-import axios from 'axios';
-
-
-
-
+import axios from "axios";
 
 definePageMeta({
-  middleware: 'auth'
+  middleware: "auth",
 });
 
 const user = reactive({
-  email: '',
-  message: ''
+  email: "",
+  message: "",
 });
 
 const { errors } = useFormValidation();
 const feedback_url = `${url}/options/feedback`;
-let seccessfully = ref('');
-
+let seccessfully = ref("");
 
 const feedbackButtonPressed = async () => {
   try {
-    seccessfully.value = '';
+    seccessfully.value = "";
     console.log("user.value - ", user);
-    
+
     const response = await axios.post(feedback_url, user);
     console.log(response.data);
 
-    const access_token = useCookie('access_token');
+    const access_token = useCookie("access_token");
     access_token.value = response.data.access_token;
     console.log(response);
     // user.email = '';
     // user.message = '';
-    seccessfully.value = 'Сообщенине отослано'
-    
+    seccessfully.value = "Сообщенине отослано";
+
     // const router = useRouter();
     // await router.push('/');
     location.reload();
   } catch (err) {
     // error.value = 'Login failed. Please check your credentials and try again.';
-    console.error('Error:', error);
+    console.error("Error:", error);
   }
 };
-
 </script>
 
 <style scoped>
-  .vac {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: flex-start;
-    height: 100vh;
-    background-color: #191919;
-    padding-top: 20px;
-  }
+.vac {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  height: 100vh;
+  background-color: #191919;
+  padding-top: 20px;
+}
 
-  .hr {
-    width: 90%;
-    height: 5px;
-    border-radius: 0%;
-    background-color: #fff;
-    margin-bottom: 40px;
-    clip-path: polygon(60% 0%, 60% 0%, 100% 100%, 0% 100%);
-  }
+.hr {
+  width: 90%;
+  height: 5px;
+  border-radius: 0%;
+  background-color: #fff;
+  margin-bottom: 40px;
+  clip-path: polygon(60% 0%, 60% 0%, 100% 100%, 0% 100%);
+}
 
-  .login-form {
+.login-form {
   background-color: transparent;
   border-radius: 8px;
   display: flex;
@@ -110,5 +125,20 @@ const feedbackButtonPressed = async () => {
 .btn:hover {
   background-color: #191919;
   border: 3px solid yellow;
+}
+
+.container{
+  display: flex;
+  gap: 40px;
+}
+.tg {
+  color: #fff;
+  font-size: 100px;
+  margin: 50px 0;
+  text-decoration: none;
+}
+
+.tg:hover {
+  color: rgb(141, 0, 143);
 }
 </style>
